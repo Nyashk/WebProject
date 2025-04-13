11f85752b1f7e3915da520af822dd642f22fbadb
Nyashk <kategrisevich@gmail.com>
2025-04-13 19:16:36 +0300

    back struck started

---
+++ back/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const db = require('./config/bd');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// проверка подключения к MySQL
+app.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT 1 + 1 AS solution');
+    res.send(`MySQL работает! Результат: ${rows[0].solution}`);
+  } catch (err) {
+    console.error('Ошибка подключения к БД:', err);
+    res.status(500).send('Ошибка подключения к базе данных');
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
