4235f13474c0f66c8046d136ed8fe4c911b0dca0
Nyashk <kategrisevich@gmail.com>
2025-04-14 00:16:32 +0300

    update

--- back/server.js
+++ back/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./config/bd');
+const db = require('./config/db');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
 // проверка подключения к MySQL
 app.get('/', async (req, res) => {
