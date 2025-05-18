3e29158e538a874fbadc3626f4e18dab28eae0af
Nyashk <kategrisevich@gmail.com>
2025-05-19 02:55:07 +0300

    add 404 page

--- artfair-front/src/App.js
+++ artfair-front/src/App.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './pages/LoginForm';
 import Register from './pages/RegisterForm';
 import MainPage from './pages/MainPage';
+import NotFoundPage from './pages/NotFoundPage';
 import Header from './components/Header';
 import './styles.css';
 
-const App = () => {
-  const showHeader = window.location.pathname !== '/login' && window.location.pathname !== '/register';
+const AppContent = () => {
+  const location = useLocation();
+  const showHeader = location.pathname !== '/login' && location.pathname !== '/register';
 
   return (
-    <div className="background">
-      <Router>
-        {showHeader && <Header />}
-        <div className="app-container">
-          <Routes>
-            <Route path="/" element={<Navigate to="/main" />} /> {/* Редирект на главную */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/main" element={<MainPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+    <>
+      {showHeader && <Header />}
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Navigate to="/main" />} /> {/* Редирект на главную */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="*" element={<NotFoundPage />} /> {/* 404 страница */}
+        </Routes>
+      </div>
+    </>
   );
 };
+
+const App = () => (
+  <div className="background">
+    <Router>
+      <AppContent />
+    </Router>
+  </div>
+);
 
 export default App;
